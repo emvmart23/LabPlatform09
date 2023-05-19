3a407a811d56d4986de74005f038d067fcf64a95
Max Martinez <max.martinez@tecsup.edu.pe>
2023-05-18 18:27:11 -0600

    first commit

--- Demo092023A/Demo092023A/MainPage.xaml.cs
+++ Demo092023A/Demo092023A/MainPage.xaml.cs
@@ -16,82 +16,22 @@ namespace Demo092023A
         {
             InitializeComponent();
 
-            /*
-           Paso 1: Declarar una instancia del gesture a utilizar
-           Paso 2: Creo un Evento
-           Paso 3: Asigno mi evento creado en el paso2
-           Paso 4: Asignarr el gesto a  un control.
-           */
-
-            var tap1 = new TapGestureRecognizer();
-            tap1.Tapped += EventTap;
-            lblDemo.GestureRecognizers.Add(tap1);
-
-            var tap2 = new TapGestureRecognizer();
-            tap2.Tapped += EventTapImage;
-            lblDemoImage.GestureRecognizers.Add(tap2);
-
-            var pinch1 = new PinchGestureRecognizer();
-            pinch1.PinchUpdated += EventPinch;
-            image1.GestureRecognizers.Add(pinch1);
-
-            var swipe1 = new SwipeGestureRecognizer();
-            swipe1.Direction = SwipeDirection.Right;
-            swipe1.Swiped += EventSwipe;            
-            image1.GestureRecognizers.Add(swipe1);
-
-
-            var swipe2 = new SwipeGestureRecognizer();
-            swipe1.Direction = SwipeDirection.Left;
-            swipe1.Swiped += EventSwipe;
-            image1.GestureRecognizers.Add(swipe1);
-
-
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PanDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PinchDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SwipeDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TapDemo());
+            };
         }
-        void EventTap(object sender, EventArgs args)
-        {
-            if (isTap)
-                btnDemo.BackgroundColor = Color.Red;
-            else
-                btnDemo.BackgroundColor = Color.Yellow;
-
-            isTap = !isTap;
-            //Business Logic   
-        }
-
-        void EventTapImage(object sender, EventArgs args)
-        {
-            
-            isVisible = !isVisible;
-            if (isVisible)
-                image1.Source = "tapped.jpg";            
-            else
-                image1.Source = "tapped2.jpg";            
-        }
-
-
-        void EventPinch(object sender, EventArgs args)
-        {
-            isVisible = !isVisible;
-            if (isVisible)
-                lblDemoImage.BackgroundColor= Color.Green;
-            else
-                lblDemoImage.BackgroundColor = Color.Orange;
-
-        }
-
-
-        void EventSwipe(object sender, EventArgs args)
-        {
-            isVisible = !isVisible;
-            if (isVisible)
-                lblDemo.BackgroundColor = Color.Green;
-            else
-                lblDemo.BackgroundColor = Color.Orange;
-        }
-
-
-
-
     }
 }
